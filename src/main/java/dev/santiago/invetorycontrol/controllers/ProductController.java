f32a263f4cdede315799a3fab4e2c33d814e1b21
Santiago <santiagopacheco34@gmail.com>
2025-09-26 16:46:31 -0300

    feat: increment getALL in controller

--- src/main/java/dev/santiago/invetorycontrol/controllers/ProductController.java
+++ src/main/java/dev/santiago/invetorycontrol/controllers/ProductController.java
@@ -1,15 +1,27 @@
 package dev.santiago.invetorycontrol.controllers;
 
+import dev.santiago.invetorycontrol.domain.Product;
+import dev.santiago.invetorycontrol.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    ProductRepository repository;
+
     @GetMapping
-    public String hello(){
-        return "Hello World!";
+    public ResponseEntity getAll(){
+        List<Product> listProduct = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listProduct);
     }
 }
