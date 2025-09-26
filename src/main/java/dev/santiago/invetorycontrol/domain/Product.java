765879a3067cd779d88a7be4bf5024ef428f1a01
Santiago <santiagopacheco34@gmail.com>
2025-09-26 13:56:33 -0300

    feat: create model

---
+++ src/main/java/dev/santiago/invetorycontrol/domain/Product.java
@@ -0,0 +1,25 @@
+package dev.santiago.invetorycontrol.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="product")
+@Table(name="product")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer ID;
+
+    private String name;
+    private Long price;
+
+
+}
