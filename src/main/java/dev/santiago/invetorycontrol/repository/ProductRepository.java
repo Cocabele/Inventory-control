69788514d3e0a30db1344f59fcc3c17d38d5bdcc
Santiago <santiagopacheco34@gmail.com>
2025-09-26 13:56:50 -0300

    feat: create repository

---
+++ src/main/java/dev/santiago/invetorycontrol/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package dev.santiago.invetorycontrol.repository;
+
+import dev.santiago.invetorycontrol.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository <Product, Integer>  {
+}
