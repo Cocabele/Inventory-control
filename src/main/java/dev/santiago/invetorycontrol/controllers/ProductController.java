20b4e8f4e93f4e9af9339a1854f0149aaf70873c
Santiago <santiagopacheco34@gmail.com>
2025-09-26 21:20:08 -0300

    feat: creating Post

--- src/main/java/dev/santiago/invetorycontrol/controllers/ProductController.java
+++ src/main/java/dev/santiago/invetorycontrol/controllers/ProductController.java
@@ -1,14 +1,13 @@
 package dev.santiago.invetorycontrol.controllers;
 
 import dev.santiago.invetorycontrol.domain.Product;
+import dev.santiago.invetorycontrol.dtos.ProductDto;
 import dev.santiago.invetorycontrol.repository.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +20,15 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity getAll(){
-        List<Product> listProduct = repository.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(listProduct);
+        List<Product> listProducts = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ProductDto dto){
+        var product = new Product();
+        BeanUtils.copyProperties(dto, product);
+        return ResponseEntity.status((HttpStatus.CREATED)).body(repository.save(product));
+
     }
 }
